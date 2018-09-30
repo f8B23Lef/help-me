b443ced1bd18584ac23b4cd3fe1c5674a6388b14
f8B23Lef <mal7rish@yandex.ru>
2018-09-30 22:16:30 +0300

    help-me/incomplete solution 11 of 17

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function count(s, pairs) {
   
   let n = 1;
-  let tmp = [];
+  let set = new Set();
   
   for(let i = 0; i < pairs.length; i++) {
     n *= pairs[i][0] ** pairs[i][1];
@@ -10,17 +10,28 @@ module.exports = function count(s, pairs) {
   if (n > 100000000) return 0;
 
   for(let k = 0; k < n; k++) {
+
+    let flag = true;
+
 		for(let j = 0; j < s.length; j++) {
-			if(s[j] === '0' && nod(k+j, n) !== 1) {
-				tmp.push(k);
-			} else if (s[j] === '1' && nod(k+j, n) === 1) {
-				tmp.push(k);
-			}
-		}
-	}
-  
-	  
-	return tmp.length % 1000000007;
+
+      let firstCondition = (s[j] === '0' && nod(k+j, n) !== 1);
+      let secondCondition = (s[j] === '1' && nod(k+j, n) === 1);
+
+      if(!(firstCondition || secondCondition)) {
+        flag = false;
+        break;
+      }
+
+    }
+    
+    if(flag) {
+      set.add(k);
+    }
+
+  }
+
+  return set.size % 1000000007;
 }
 
 function nod(a, b) {
